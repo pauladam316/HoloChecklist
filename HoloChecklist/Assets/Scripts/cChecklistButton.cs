1a3486694001f5d31cdbb916b8328ba2f922ca99
Adam Paul <pauladam316@gmail>
2017-08-12 21:42:34 -0400

    Added checklist button implementation and art assets

---
+++ HoloChecklist/Assets/Scripts/cChecklistButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class cChecklistButton: MonoBehaviour
+{
+	private GestureRecognizer gestureRecognizer;                                    //Used for recognizing hololens air tap
+	private Color mNormalColor = Color.white;										//unhighlighted color
+	private Color mHighlightedColor = new Color(0.380f, 0.380f, 0.380f);
+	public event EventHandler OnPropertyChanged;									//handle button presses
+
+	public bool Highlighted = false;
+
+	private void Start()
+	{
+		gestureRecognizer = new GestureRecognizer();                                //Setup Gesture Recognizer
+		gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+		gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
+		gestureRecognizer.StartCapturingGestures();
+	}
+
+	private void Update()
+	{
+		if (!Highlighted && LookingAtButton())										//highlight if we are looking at it
+		{
+			Highlighted = true;
+			HighlightButton();
+		}
+		else if (Highlighted && !LookingAtButton())
+		{
+			UnhighlightButton();													//otherwise dont
+			Highlighted = false;
+		}
+	}
+
+	private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+	{
+		if (Highlighted)
+		{
+			OnPropertyChanged(this, new EventArgs());
+		}
+	}
+
+	/**
+	 * HighlightButton
+	 * Changes the color to the selected color
+	 */
+	private void HighlightButton()
+	{
+		GetComponent<Renderer>().material.SetColor("_Tint", mHighlightedColor);
+	}
+
+	/**
+	 * UnhighlightButton
+	 * Changes the color to the unselected color
+	 */
+	private void UnhighlightButton()
+	{
+		GetComponent<Renderer>().material.SetColor("_Tint", mNormalColor);
+	}
+
+	/**
+	 * LookingAtButton
+	 * Checks to see if the camera transform is looking at the button specified
+	 * @return if it is looking at the button
+	 */
+	private bool LookingAtButton()
+	{
+		RaycastHit[] hits;
+
+		hits = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward);
+
+		foreach (RaycastHit hit in hits)
+		{
+			if (hit.transform.gameObject == this.gameObject)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ HoloChecklist/Assets/Shaders/shMeshStandard.shader
@@ -0,0 +1,85 @@
+﻿// Upgrade NOTE: replaced 'UNITY_INSTANCE_ID' with 'UNITY_VERTEX_INPUT_INSTANCE_ID'
+
+Shader "Unlit/Standard Unlit"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Color("Main Color", Color) = (1,1,1,1)
+		_Tint("Selection Tint", Color) = (1,1,1,1)
+
+		[HideInInspector] _Cutoff("Alpha cutoff", Range(0,1)) = 0
+		[HideInInspector] _Mode("__mode", Float) = 0
+		[HideInInspector] _SrcBlend("__src", Float) = 1.0
+		[HideInInspector] _DstBlend("__dst", Float) = 0.0
+		[HideInInspector] _ZWrite("__zw", Int) = 1.0
+	}  
+	SubShader
+	{	
+		Pass
+		{
+			Tags{ "RenderType" = "Opaque" "IgnoreProjector" = "True" }
+			LOD 100
+
+			ZWrite [_ZWrite]
+			Blend[_SrcBlend][_DstBlend]
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_instancing
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				fixed4 vertex : POSITION;
+				fixed2 uv : TEXCOORD0;
+				fixed4 color : COLOR;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct v2f
+			{
+				fixed2 uv : TEXCOORD0;
+				fixed4 vertex : SV_POSITION;
+				fixed4 color : COLOR;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			sampler2D _MainTex;
+			fixed4 _MainTex_ST;
+			fixed4 _Color; 
+			fixed4 _Tint;
+			fixed _Cutoff;
+
+			UNITY_INSTANCING_CBUFFER_START(MyProperties)
+			UNITY_INSTANCING_CBUFFER_END
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+
+				UNITY_SETUP_INSTANCE_ID(v);
+				UNITY_TRANSFER_INSTANCE_ID(v, o);
+
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.color = v.color;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : Color
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				clip(col.a - _Cutoff);
+				col *= _Color;
+				col *= i.color;
+				col *= _Tint;
+				UNITY_SETUP_INSTANCE_ID(i);
+				return col;
+			}
+			ENDCG
+		}
+	}
+CustomEditor "ShaderRenderMode"
+}
