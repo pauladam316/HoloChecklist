2a95c1d039b9565d85bd0a576145d296a17ce984
Adam Paul <pauladam316@gmail>
2017-08-12 21:42:34 -0400

    Added checklist button implementation and art assets

---
+++ HoloChecklist/Assets/Scripts/cChecklistHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cChecklistHandler : MonoBehaviour {
+
+	public cChecklistButton ExpandButton;
+	
+
+	// Use this for initialization
+	void Start () {
+		ExpandButton.OnPropertyChanged += ExpandButton_OnPropertyChanged;
+	}
+
+	private void ExpandButton_OnPropertyChanged(object sender, EventArgs e)
+	{
+		throw new NotImplementedException();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void ExpandButton_Click(object sender)
+	{
+
+	}
+}
